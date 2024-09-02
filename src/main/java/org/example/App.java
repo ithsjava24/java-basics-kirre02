12faf9390290d06cff2075bfc782511fbb801696
kirre02 <74262686+kirre02@users.noreply.github.com>
2024-09-02 13:55:40 +0200

    feat: skapade en menu

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,40 @@
 package org.example;
 
+import javax.xml.crypto.Data;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+
+        var selection = menu();
+
+        switch (selection) {
+            case 1 -> System.out.println("Inmatning");
+            case 2 -> System.out.println("Min, Max och Medel");
+            case 3 -> System.out.println("Sortera");
+            case 4 -> exit(1);
+            default -> System.out.println("ogiltigt val");
+        };
+
+    }
+
+    private static int menu() {
+        int selection;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Elpriser");
+        System.out.println("-------------------------\n");
+        System.out.println("1 - inmatning");
+        System.out.println("2 - Min, Max och Medel");
+        System.out.println("3 - Sortera");
+        System.out.println("4 - Avsluta");
+
+        selection = sc.nextInt();
+
+        return selection;
     }
 }
